66f7bf39a942970712c63070325b456fc9bf3a37
adityachaudhary <aditya.ch0603@gmail.com>
2024-07-04 18:04:08 +0530

    Set up Google OAuth and redirect to welcome.html

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ app.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
     // Successful authentication, redirect home.
-    res.redirect('/');
+    res.redirect('/welcome.html');
   });
 
 // Start the server and listen on the specified port
